80f0a55cdb48b1df4e4a146a3b7d86561f822c25
hiroraba <xornelius0313@gmail.com>
2015-07-26 15:09:33 +0900

    Initial Commit

---
+++ VRTakeMovie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VRTakeMovie
+//
+//  Created by matsuohiroki on 2015/07/26.
+//  Copyright © 2015年 matsuohiroki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
